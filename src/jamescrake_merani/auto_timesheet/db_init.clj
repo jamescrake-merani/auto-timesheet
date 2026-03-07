7d2475e0a0129568e607bca546e74452f361a426
James Crake-Merani <james@jamescm.co.uk>
2026-03-07 17:02:22 +0000

    Started open database function.

--- src/jamescrake_merani/auto_timesheet/db_init.clj
+++ src/jamescrake_merani/auto_timesheet/db_init.clj
@@ -1,7 +1,12 @@
 (ns jamescrake-merani.auto-timesheet.db-init
-  (:require [jamescrake-merani.auto-timesheet.db :as d]))
+  (:require [jamescrake-merani.auto-timesheet.db :as d]
+            [hugsql.core :as h]
+            [hugsql.adapter.next-jdbc :as next-adapter]))
 
 (defn init-database [db]
   (d/create-category-table db)
   (d/create-clock-in-table db)
   (d/create-clock-out-table db))
+
+(defn open-database [db-path]
+  (h/set-adapter! (next-adapter/hugsql-adapter-next-jdbc)))
