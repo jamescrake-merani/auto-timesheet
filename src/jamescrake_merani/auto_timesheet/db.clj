a78581e622bf7af6a75cc770a2b69090062bcc4e
James Crake-Merani <james@jamescm.co.uk>
2026-03-07 16:59:04 +0000

    Fix file path.

--- src/jamescrake_merani/auto_timesheet/db.clj
+++ src/jamescrake_merani/auto_timesheet/db.clj
@@ -1,4 +1,4 @@
 (ns jamescrake-merani.auto-timesheet.db
   (:require [hugsql.core :as hugsql]))
 
-(hugsql/def-db-fns "jamescrake-merani/auto-timesheet/sql/clocking.sql")
+(hugsql/def-db-fns "jamescrake_merani/auto_timesheet/sql/clocking.sql")
