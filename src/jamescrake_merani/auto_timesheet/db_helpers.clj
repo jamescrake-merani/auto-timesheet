d98dcce1c8fdc4ab22619fbcb77030f80e4bad9d
James Crake-Merani <james@jamescm.co.uk>
2026-03-28 20:18:31 +0000

    Specify parameter correctly.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -3,6 +3,6 @@
 
 (defn clock-in [category]
   (cond (integer? category) (as-db/clock-in {:category-id category})
-        (or (keyword? category) (string? category)) (as-db/clock-in {:category-id (as-db/get-category-from-name category)})
+        (or (keyword? category) (string? category)) (as-db/clock-in {:category-id (as-db/get-category-from-name {:name category})})
         :else (throw (.Exception "Category needs to be an id, or a name."))))
 
