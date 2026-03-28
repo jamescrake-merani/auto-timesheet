31f31671192a30fdde3307aa1a1e5684c1907362
James Crake-Merani <james@jamescm.co.uk>
2026-03-28 20:39:22 +0000

    Formatting changes & extract category id.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -2,7 +2,11 @@
   (:require [jamescrake-merani.auto-timesheet.db :as as-db]))
 
 (defn clock-in [db category]
-  (cond (integer? category) (as-db/clock-in db {:category-id category})
-        (or (keyword? category) (string? category)) (as-db/clock-in db {:category-id (as-db/get-category-from-name db {:name category})})
-        :else (throw (.Exception "Category needs to be an id, or a name."))))
+  (cond (integer? category)
+        (as-db/clock-in db {:category-id category})
+        (or (keyword? category) (string? category))
+        (as-db/clock-in db {:category-id
+                            {:categoryid (as-db/get-category-from-name db {:name category})}})
+        :else
+        (throw (.Exception "Category needs to be an id, or a name."))))
 
