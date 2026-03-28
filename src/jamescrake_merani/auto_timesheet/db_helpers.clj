38f7958f679624d6951cc0900144516ae88a4cdf
James Crake-Merani <james@jamescm.co.uk>
2026-03-28 20:32:31 +0000

    Missing db here as well.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -3,6 +3,6 @@
 
 (defn clock-in [db category]
   (cond (integer? category) (as-db/clock-in db {:category-id category})
-        (or (keyword? category) (string? category)) (as-db/clock-in {:category-id (as-db/get-category-from-name db {:name category})})
+        (or (keyword? category) (string? category)) (as-db/clock-in db {:category-id (as-db/get-category-from-name db {:name category})})
         :else (throw (.Exception "Category needs to be an id, or a name."))))
 
