69e3fbdb77ef5a5af4ceba74d02b0736e37f7e7c
James Crake-Merani <james@jamescm.co.uk>
2026-05-24 13:00:40 +0100

    Missing db param.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -14,5 +14,5 @@
 
 (defn clock-out
   ([db] (clock-out db (-> (as-db/hanging-clockins db) first :clockinid)))
-  ([db clockin-id] (as-db/clock-out {:clockinid clockin-id})))
+  ([db clockin-id] (as-db/clock-out db {:clockinid clockin-id})))
 
