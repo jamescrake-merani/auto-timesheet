390c193e7a02096e1b8eb3d835c493d136c3d25d
James Crake-Merani <james@jamescm.co.uk>
2026-05-24 11:16:07 +0100

    Missing db.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -7,7 +7,7 @@
         (or (keyword? category) (string? category))
         (let [category-id (as-db/get-category-from-name db {:name category})]
           (if (nil? category-id)
-            (clock-in (:categoryid (as-db/create-category category)))
+            (clock-in (:categoryid (as-db/create-category db category)))
             (as-db/clock-in db {:category-id (:categoryid (as-db/get-category-from-name db {:name category}))})))
         :else
         (throw (.Exception "Category needs to be an id, or a name."))))
