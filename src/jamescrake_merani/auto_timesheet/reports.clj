aa1fc2e47f93d132382f8db822a2da9846a4827c
James Crake-Merani <james@jamescm.co.uk>
2026-06-03 14:49:30 +0100

    Reports clj file (with todo comment)

---
+++ src/jamescrake_merani/auto_timesheet/reports.clj
@@ -0,0 +1,4 @@
+(ns jamescrake-merani.auto-timesheet.reports)
+
+;; TODO: Before starting report, need to have an SQL query that will get all the
+;; clock ins, and outs (through join), then want to group them by day probably.
