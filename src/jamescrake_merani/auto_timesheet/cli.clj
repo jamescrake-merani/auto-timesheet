02da1aab04a4d3e16c71194e15dced3fe4e415ce
James Crake-Merani <james@jamescm.co.uk>
2026-06-06 11:09:07 +0100

    Add command for when no command is mentioned.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -11,9 +11,13 @@
 (defn clockin [_]
   (println "Clock in"))
 
+(defn no-command [_]
+  (println "You need to use a command."))
+
 (def table
   [{:cmds ["clockin"] :fn clockin :doc "Clock in" :spec clock-spec}
-   {:cmds ["clockout"] :fn clockout :doc "Clock out" :spec clock-spec}])
+   {:cmds ["clockout"] :fn clockout :doc "Clock out" :spec clock-spec}
+   {:cmds [] :fn no-command :doc "No command"}])
 
 (defn -main [& args]
   (cli/dispatch table args))
