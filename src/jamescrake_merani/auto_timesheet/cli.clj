ef7adb1a0c32de44234185264efcd77407212e41
James Crake-Merani <james@jamescm.co.uk>
2026-06-06 11:21:21 +0100

    Load the db.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -1,6 +1,11 @@
 (ns jamescrake-merani.auto-timesheet.cli
-  (:require [babashka.cli :as cli])
+  (:require [babashka.cli :as cli]
+            [jamescrake-merani.auto-timesheet.db-init :refer [open-database]]
+            [clojure.java.io :as io])
+  (:import (dev.dirs ProjectDirectories))
   (:gen-class))
+
+(def directories (ProjectDirectories/from "me" "jamescrake-merani" "auto-timesheet"))
 
 (def clock-spec
   {:category {:alias :c}})
@@ -19,6 +24,8 @@
    {:cmds ["clockout"] :fn clockout :doc "Clock out" :spec clock-spec}
    {:cmds [] :fn no-command :doc "No command"}])
 
+;; TODO: Might only want to init the db for some commands later.
 (defn -main [& args]
-  (cli/dispatch table args))
+  (let [db (open-database (io/file (.dataDir directories) "data.db"))]
+    (cli/dispatch table args)))
 
