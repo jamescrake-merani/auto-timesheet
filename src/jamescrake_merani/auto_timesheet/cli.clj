2dae97bebe65629da0b5c7ba4d108b5f93b4e99c
James Crake-Merani <james@jamescm.co.uk>
2026-06-06 10:52:50 +0100

    Add dummy cli.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -1,9 +1,20 @@
 (ns jamescrake-merani.auto-timesheet.cli
   (:require [babashka.cli :as cli])
-  (:import (java.time LocalTime))
   (:gen-class))
 
-(comment
-  (LocalTime/parse "12:00:13"))
+(def clock-spec
+  {:category {:alias :c}})
 
+(defn clockout [_]
+  (println "Clock out"))
+
+(defn clockin [_]
+  (println "Clock in"))
+
+(def table
+  [{:cmds ["clockin"] :fn clockin :doc "Clock in" :spec clock-spec}
+   {:cmds ["clockout"] :fn clockout :doc "Clock out" :spec clock-spec}])
+
+(defn -main [& args]
+  (cli/dispatch table args))
 
