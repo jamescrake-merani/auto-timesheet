3278baa358840f66a43fdab06fddd710f83014d4
James Crake-Merani <james@jamescm.co.uk>
2026-06-10 21:52:22 +0100

    Add a test for clockin in, and out.

--- test/jamescrake_merani/auto_timesheet/db_tests.clj
+++ test/jamescrake_merani/auto_timesheet/db_tests.clj
@@ -1,6 +1,14 @@
 (ns jamescrake-merani.auto-timesheet.db-tests
   (:require [jamescrake-merani.auto-timesheet.db-helpers :as sut]
+            [jamescrake-merani.auto-timesheet.db :as db-raw]
             [jamescrake-merani.auto-timesheet.db-init :as db-init]
             [clojure.test :as t]))
 
 (def db (db-init/open-database ":memory:"))
+
+(t/deftest clockin-clockout-test
+  (t/is (count (db-raw/hanging-clockins db)) 0)
+  (sut/clock-in db "test")
+  (t/is (count (db-raw/hanging-clockins db)) 1)
+  (sut/clock-out db)
+  (t/is (count (db-raw/hanging-clockins db)) 0))
