926d09434a05495598c1e8b1b56440acc66c5ada
James Crake-Merani <james@jamescm.co.uk>
2026-06-10 21:33:06 +0100

    Create an in memory db.

--- test/jamescrake_merani/auto_timesheet/db_tests.clj
+++ test/jamescrake_merani/auto_timesheet/db_tests.clj
@@ -1,5 +1,6 @@
 (ns jamescrake-merani.auto-timesheet.db-tests
   (:require [jamescrake-merani.auto-timesheet.db-helpers :as sut]
+            [jamescrake-merani.auto-timesheet.db-init :as db-init]
             [clojure.test :as t]))
 
-
+(def db (db-init/open-database ":memory:"))
