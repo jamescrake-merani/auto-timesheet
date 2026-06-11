9905dac09ca8b5c4a806fa33a9c2650ed4a56bc3
James Crake-Merani <james@jamescm.co.uk>
2026-06-11 09:50:17 +0100

    Init the db just in this test.
    So it gets initalised separately for each test.

--- test/jamescrake_merani/auto_timesheet/db_test.clj
+++ test/jamescrake_merani/auto_timesheet/db_test.clj
@@ -4,11 +4,11 @@
             [jamescrake-merani.auto-timesheet.db-init :as db-init]
             [clojure.test :as t]))
 
-(def db (db-init/open-database ":memory:"))
-
 (t/deftest clockin-clockout-test
-  (t/is (count (db-raw/hanging-clockins db)) 0)
-  (sut/clock-in db "test")
-  (t/is (count (db-raw/hanging-clockins db)) 1)
-  (sut/clock-out db)
-  (t/is (count (db-raw/hanging-clockins db)) 0))
+  (let [db (db-init/open-database ":memory:")]
+    (t/is (count (db-raw/hanging-clockins db)) 0)
+    (sut/clock-in db "test")
+    (t/is (count (db-raw/hanging-clockins db)) 1)
+    (sut/clock-out db)
+    (t/is (count (db-raw/hanging-clockins db)) 0)))
+
