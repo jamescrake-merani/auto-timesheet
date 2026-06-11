58aa0f5acdb1ac4530e18a6d1f9c81685980fb15
James Crake-Merani <james@jamescm.co.uk>
2026-06-11 11:41:22 +0100

    Remove todo comment.

--- test/jamescrake_merani/auto_timesheet/db_test.clj
+++ test/jamescrake_merani/auto_timesheet/db_test.clj
@@ -51,7 +51,6 @@
     (let [db (db-init/open-database ":memory:")]
       (db-raw/create-category db {:name "test"})
       (sut/manual-entry db (.toLocalTime (first datum)) (.toLocalTime (second datum)) "test")
-      ;; TODO: This is repetitive.
       (let [full-clock (first (db-raw/clocks-within-timeperiod
                                db {:periodstart (LocalDateTime/of (LocalDate/now) (LocalTime/of 0 0))
                                    :periodend (LocalDateTime/of (LocalDate/now) (LocalTime/of 23 59))}))]
