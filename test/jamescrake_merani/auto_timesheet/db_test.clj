0fa7492b2c756debb52efe084026760c91155796
James Crake-Merani <james@jamescm.co.uk>
2026-06-11 11:04:28 +0100

    Create a separate list for test data.

--- test/jamescrake_merani/auto_timesheet/db_test.clj
+++ test/jamescrake_merani/auto_timesheet/db_test.clj
@@ -14,17 +14,21 @@
     (sut/clock-out db)
     (t/is (count (db-raw/hanging-clockins db)) 0)))
 
+(def duration-test-data
+  [[(LocalDateTime/of 2026 6 11 10 00) (LocalDateTime/of 2026 6 11 12 00) 120]])
+
 (t/deftest clockin-duration-test
-  (let [db (db-init/open-database ":memory:")]
-    (sut/clock-in db "test" (LocalDateTime/of 2026 6 11 10 00))
-    (sut/clock-out db (LocalDateTime/of 2026 6 11 12 00))
-    (let [full-clock (first
-                      (db-raw/clocks-within-timeperiod
-                       db {:periodstart (LocalDateTime/of 2026 6 11 0 0)
-                           :periodend (LocalDateTime/of 2026 6 11 23 59)}))]
-      (t/is
-       (.toMinutes
-        (Duration/between (LocalDateTime/parse (:starttime full-clock))
-                          (LocalDateTime/parse (:stoptime full-clock))))
-       120))))
+  (doseq [datum duration-test-data]
+    (let [db (db-init/open-database ":memory:")]
+      (sut/clock-in db "test" (first datum))
+      (sut/clock-out db (second datum))
+      (let [full-clock (first
+                        (db-raw/clocks-within-timeperiod
+                         db {:periodstart (LocalDateTime/of 2026 6 11 0 0)
+                             :periodend (LocalDateTime/of 2026 6 11 23 59)}))]
+        (t/is
+         (.toMinutes
+          (Duration/between (LocalDateTime/parse (:starttime full-clock))
+                            (LocalDateTime/parse (:stoptime full-clock))))
+         (nth datum 2))))))
 
