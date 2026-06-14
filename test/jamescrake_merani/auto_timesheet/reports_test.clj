2f7636b35ed1da5fa099d07adfd02f3fbc29fa20
James Crake-Merani <james@jamescm.co.uk>
2026-06-14 20:03:58 +0100

    Test for human readable report.

--- test/jamescrake_merani/auto_timesheet/reports_test.clj
+++ test/jamescrake_merani/auto_timesheet/reports_test.clj
@@ -1,6 +1,34 @@
 (ns jamescrake-merani.auto-timesheet.reports-test
   (:require [jamescrake-merani.auto-timesheet.reports :as sut]
-            [clojure.test :as t]))
+            [jamescrake-merani.auto-timesheet.db-init :as db-init]
+            [jamescrake-merani.auto-timesheet.db-helpers :as helpers]
+            [clojure.test :as t])
+  (:import (java.time LocalDateTime)))
 
-
+(t/deftest human-readable-report-test
+  (let [db (db-init/open-database ":memory:")]
+    (helpers/manual-entry db
+                          (LocalDateTime/parse "2026-06-08T10:00")
+                          (LocalDateTime/parse "2026-06-08T12:00")
+                          "work")
+    (helpers/manual-entry db
+                          (LocalDateTime/parse "2026-06-08T13:00")
+                          (LocalDateTime/parse "2026-06-08T16:00")
+                          "work")
+    (helpers/manual-entry db
+                          (LocalDateTime/parse "2026-06-11T02:00")
+                          (LocalDateTime/parse "2026-06-11T09:30")
+                          "work")
+    (helpers/manual-entry db
+                          (LocalDateTime/parse "2026-06-11T15:10")
+                          (LocalDateTime/parse "2026-06-11T16:15")
+                          "work")
+    (t/is (sut/human-readable-report db)
+          "Monday
+10:00-12:00 (2 hours, 0 minutes)
+13:00-16:00 (3 hours, 0 minutes)
+Thursday
+02:00-09:30 (7 hours, 30 minutes)
+15:10-16:15 (1 hours, 5 minutes)
+")))
 
