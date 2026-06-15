dc0fbc95c165fba4473c16fafadfd555425a9002
James Crake-Merani <james@jamescm.co.uk>
2026-06-15 16:17:21 +0100

    Use into instead of conj.

--- src/jamescrake_merani/auto_timesheet/reports.clj
+++ src/jamescrake_merani/auto_timesheet/reports.clj
@@ -30,7 +30,7 @@
 ;; TODO: Add weekly total.
 (defn human-readable-summary [grouped-clocks]
   (reduce-kv (fn [lines day clocks]
-               (conj (day-summary day clocks) lines))
+               (into lines (day-summary day clocks)))
              [] grouped-clocks))
 
 ;; TODO: Reports should be able to take in parameters. For now, we need to use
