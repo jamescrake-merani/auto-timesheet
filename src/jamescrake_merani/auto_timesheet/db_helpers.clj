88e2ed1d8d867f157506128b4bb9bf6b0c86f577
James Crake-Merani <james@jamescm.co.uk>
2026-06-18 15:16:47 +0100

    Need to parse into local date time.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -75,5 +75,5 @@
 
 ;; Returns duration.
 (defn sum-clocks [clocks]
-  (reduce #(.plus %1 (Duration/between (:starttime %2) (:stoptime %2))) Duration/ZERO clocks))
+  (reduce #(.plus %1 (Duration/between (LocalDateTime/parse (:starttime %2)) (LocalDateTime/parse (:stoptime %2)))) Duration/ZERO clocks))
 
