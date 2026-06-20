7811cf2f01b271af6913a00279a2b05dc330ee8a
James Crake-Merani <james@jamescm.co.uk>
2026-06-20 11:54:20 +0100

    Function to load config.

--- src/jamescrake_merani/auto_timesheet/config.clj
+++ src/jamescrake_merani/auto_timesheet/config.clj
@@ -1,9 +1,17 @@
 (ns jamescrake-merani.auto-timesheet.config
   (:import (dev.dirs ProjectDirectories))
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
 (def ^ProjectDirectories directories (ProjectDirectories/from "me" "jamescrake-merani" "auto-timesheet"))
 
 (def default-config
   {:default-category nil
    :sql-directory (io/file (.dataDir directories) "data.dir")})
+
+(defn load-config
+  (->>
+   (io/file (.configDir directories) "config.edn")
+   slurp
+   edn/read-string
+   (merge default-config)))
