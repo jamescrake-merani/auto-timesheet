944fe0f3aa631e9f24d22fb941416b7f8979097e
James Crake-Merani <james@jamescm.co.uk>
2026-06-21 13:55:41 +0100

    Error if the category doesn't exist.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -67,6 +67,10 @@
                           (constantly true)
                           #(= (:categoryid %) category-id))
         report-function (get reports-available (keyword type))]
+    (if (and category (nil? category-id))
+      (do
+        (.println ^java.io.PrintWriter *err* "That category does not exist.")
+        (System/exit 1)))
     (if (nil? report-function)
       (do
         (.println ^java.io.PrintWriter *err* "That report type does not exist.")
