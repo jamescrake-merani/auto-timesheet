043429ea6edb6783675950e2e63a11ea9799aaeb
James Crake-Merani <james@jamescm.co.uk>
2026-07-23 12:43:16 +0100

    Make parents for the db path.

--- src/jamescrake_merani/auto_timesheet/db_init.clj
+++ src/jamescrake_merani/auto_timesheet/db_init.clj
@@ -2,7 +2,8 @@
   (:require [jamescrake-merani.auto-timesheet.db :as d]
             [hugsql.core :as h]
             [hugsql.adapter.next-jdbc :as next-adapter]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [clojure.java.io :as io]))
 
 (defn init-database [db]
   (d/create-category-table db)
@@ -12,6 +13,8 @@
 ;; TODO: Might be better just to take the project directory, and work out where
 ;; the db should be in there.
 (defn open-database [db-path]
+  ;; This ensures the path for the db actually exists before its created.
+  (io/make-parents db-path)
   (h/set-adapter! (next-adapter/hugsql-adapter-next-jdbc))
   (let [conn (jdbc/get-connection {:dbtype "sqlite" :dbname db-path})]
     (init-database conn)
