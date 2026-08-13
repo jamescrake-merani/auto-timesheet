cc5dea3bf8f403b604f70ff9f8f538c9a69697c6
James Crake-Merani <james@jamescm.co.uk>
2026-08-13 18:19:00 +0100

    Clock in takes in a timestamp as well.
    Also abstract the datetime logic into its own function.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -43,14 +43,17 @@
    :time {:alias :t
           :desc "The effective time of the clock. If not specified, then this will be the current time according to your system."}})
 
+(defn- get-effective-datetime [time-from-user]
+  (if time-from-user
+    (LocalDateTime/of (LocalDate/now) (LocalTime/parse time-from-user))
+    (LocalTime/now)))
+
 ;: TODO: Probably want to be able to provide a category.
 (defn clockout [{{:keys [category time]} :opts}]
   (let [hanging-clockins (helpers/hanging-clockins @db)
         time-since-clockin (when (not (empty? hanging-clockins))
                              (:starttime (first hanging-clockins)))
-        effective-datetime (if time
-                             (LocalDateTime/of (LocalDate/now) (LocalTime/parse time))
-                             (LocalTime/now))]
+        effective-datetime (get-effective-datetime time)]
     (cond (empty? hanging-clockins)
           (do
             (.println ^java.io.PrintWriter *err* "You are not clocked in.")
@@ -68,15 +71,16 @@
 
 ;: TODO Allow the user to disable this check.
 ;; TODO: Also this check only looks for all categories not one specific one.
-(defn clockin [{{:keys [category force]} :opts}]
-  (let [category-to-use (or category (:default-category @config))]
+(defn clockin [{{:keys [category force time]} :opts}]
+  (let [category-to-use (or category (:default-category @config))
+        effective-datetime (get-effective-datetime time)]
     (cond
       (not (or (empty? (helpers/hanging-clockins @db)) force)) (println "You are already clocked in. (use the --force flag to ignore this check.)")
       ;; TODO: Probably want to explain a bit better how to add a default one - perhaps link to documentation when thats available?
       (nil? category-to-use) (do (.println ^java.io.PrintWriter *err*  "You need to provide a category with clock ins as you haven't provided a default one in your config.")
                                  (System/exit 1))
       :else (do
-              (helpers/clock-in @db category-to-use)
+              (helpers/clock-in @db category-to-use effective-datetime)
               (println "Clocked in.")))))
 
 (def report-spec
