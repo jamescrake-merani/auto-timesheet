bb4848b35442d783f61573174c13a886903af31c
James Crake-Merani <james@jamescm.co.uk>
2026-08-15 17:00:16 +0100

    Fixed syntax mistake.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -217,7 +217,7 @@
   [{:cmds ["clockin"] :fn clockin :doc "Make a clock in." :spec clock-spec}
    {:cmds ["clockout"] :fn clockout :doc "Make a clock out." :spec clock-spec}
    {:cmds ["report"] :fn report :doc "Display reports" :spec report-spec}
-   {:cmds ["reports-available" :fn print-reports-available :doc "Shows all the reports that are available in this build."]}
+   {:cmds ["reports-available"] :fn print-reports-available :doc "Shows all the reports that are available in this build."}
    {:cmds ["status"] :fn status-command :doc "Shows current clock in status"}
    {:cmds ["delete-range"] :fn delete-range-command :spec range-spec :doc "Deletes clocks within a specified range during today."}
    {:cmds ["manual-entry"] :fn manual-entry :spec manual-entry-spec :doc "Manually make a clock in, and clock out."}
