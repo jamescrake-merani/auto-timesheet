c60cb48482b8a8388cf79a542d10b099747ef9af
James Crake-Merani <james@jamescm.co.uk>
2026-08-15 15:29:02 +0100

    Make this message a bit more specific.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -106,11 +106,11 @@
 
 (defn print-status []
   (println
-   (format "You have worked %s today"
-            (-> @db
-                (helpers/clocks-within-date (LocalDate/now))
-                helpers/sum-clocks
-                format-duration)))
+   (format "You have clocked in %s today."
+           (-> @db
+               (helpers/clocks-within-date (LocalDate/now))
+               helpers/sum-clocks
+               format-duration)))
   (let [hanging-clockins (helpers/hanging-clockins @db)]
     (cond (empty? hanging-clockins) (println "You are not currently clocked in.")
           (= (count hanging-clockins) 1) (println (format-clockin (first hanging-clockins) true))
