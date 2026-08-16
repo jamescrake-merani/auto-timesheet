75dfc655768752f12c2f8c40785cfbc59c77cef1
James Crake-Merani <james@jamescm.co.uk>
2026-08-16 12:09:22 +0100

    Remove this old function.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -143,11 +143,6 @@
       (as-db/amend-clockout db {:newstoptime (.toEpochSecond newtime)
                                 :oldstoptime (.toEpochSecond oldtime)}))))
 
-;; For these two functions: clock should be converted with `convert-clock`
-(defn amend-clockin [db clock ^LocalDateTime new-start-date]
-  (as-db/amend-clockin db {:newstarttime (.toEpochSecond new-start-date)
-                           :oldstarttime (.toEpochSecond ^LocalDateTime (:starttime clock))}))
-
 ;; Returns duration.
 (defn sum-clocks [clocks]
   (reduce #(.plus ^java.time.Duration %1
