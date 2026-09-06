7390cd8f2967bdb85603059419c969586232defd
James Crake-Merani <james@jamescm.co.uk>
2026-09-06 21:44:52 +0100

    Add docstrings to functions.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -25,13 +25,21 @@
                       ZoneOffset)
            (java.time.temporal ChronoUnit)))
 
-(defn- to-epoch [^LocalDateTime ldt]
+(defn- to-epoch
+  "Converts `ldt` to seconds since the UTC epoch."
+  [^LocalDateTime ldt]
   (.toEpochSecond ldt ZoneOffset/UTC))
 
-(defn from-epoch [epoch-seconds]
+(defn from-epoch
+  "Converts `epoch-seconds` into a Java LocalDateTime"
+  [epoch-seconds]
   (LocalDateTime/ofInstant (Instant/ofEpochSecond epoch-seconds) ZoneOffset/UTC))
 
-(defn convert-clock [clock]
+(defn convert-clock
+  "Takes in a `clock` map that has come straight from
+  the database, and makes sure any timestamps are converted so that they use the
+   Java.Date API data structures."
+  [clock]
   (into {}
         (map (fn [[key value]]
                (if (contains? #{:starttime :stoptime} key)
@@ -39,6 +47,10 @@
                  [key value])) clock)))
 
 (defn clock-in
+  "Make a clock in. `category` needs to be specified, and optionally
+  `current-timestamp` can be specified if the clock in is to be made at a
+  specific time. Otherwise, `current-timestamp` is taken to be the current
+  time."
   ([db category] (clock-in db category (LocalDateTime/now)))
   ([db category current-timestamp]
    (cond (integer? category)
@@ -55,10 +67,16 @@
 (defn- hanging-clockin-id [db]
   (-> (as-db/hanging-clockins db) first :clockinid))
 
-(defn hanging-clockins [db]
+(defn hanging-clockins
+  "Checks to see if there are any clock ins that don't have an associated clock out."
+  [db]
   (map convert-clock (as-db/hanging-clockins db)))
 
 (defn clock-out
+  "Make a clock out. Without providing any arguments, the clock out will be
+  attached to the last clock in, but you can optionally a specific clock in id.
+  The clock out is taken to be `current-timestamp` if specified. Otherwise, the
+  current time will be used."
   ([db] (clock-out db (hanging-clockin-id db) (LocalDateTime/now)))
   ([db clockin-id-or-timestamp]
    (if (integer? clockin-id-or-timestamp)
@@ -75,6 +93,8 @@
 
 ;; TODO: Doesn't do the same category checks as `clock-in`
 (defn manual-entry
+  "Manually create a clock in, and clock out in one go by specifying the times for
+  clock in, and clock out."
   [db clockin-time clockout-time category]
   (let [category-id (:categoryid (as-db/get-category-from-name db {:name category}))
         ;; TODO: At the moment this assumes that clockin-time, and clockout-time
@@ -85,12 +105,15 @@
                                :category-id category-id
                                :clockoutid (:clockoutid (as-db/clock-out db {:stoptime (to-epoch clockout-stoptime)}))})))
 
-(defn delete-clocks [db period-start period-end]
+(defn delete-clocks
+  "Delete all clock ins, and clock outs which lie within `period-start`, and `period-end`."
+  [db period-start period-end]
   (as-db/delete-clockouts-within-timeperiod db {:periodstart (to-epoch period-start)
                                                 :periodend (to-epoch period-end)})
   (as-db/delete-clockins-within-timeperiod db {:periodstart (to-epoch period-start)
                                                :periodend (to-epoch period-end)}))
 (defn clocks-within-timeperiod
+  "Return all the clocks that fill within `period-start`, and `period-end`."
   ([db period-start period-end]
    (clocks-within-timeperiod db period-start period-end true))
   ([db period-start period-end use-starttime?]
@@ -101,12 +124,16 @@
           :periodend (to-epoch period-end)
           :timeparam (if use-starttime? "starttime" "stoptime")}))))
 
-(defn clocks-within-date [db ^LocalDate date]
+(defn clocks-within-date
+  "Get all the clocks within the day specifed by `date`"
+  [db ^LocalDate date]
   (clocks-within-timeperiod db
                             (LocalDateTime/of date LocalTime/MIDNIGHT)
                             (LocalDateTime/of (.plusDays date 1) LocalTime/MIDNIGHT)))
 
 (defn clocks-in-week
+  "Get all the clocks with the week of `date`. If `date` is not specified, then
+  get all the clocks within the current week."
   ([db] (clocks-in-week db (LocalDateTime/now)))
   ([db ^LocalDateTime date]
    (let [^LocalDateTime period-beginning (-> date
@@ -115,17 +142,24 @@
          period-end (.plusWeeks period-beginning 1)]
      (clocks-within-timeperiod db period-beginning period-end))))
 
-(defn all-clocks [db]
+(defn all-clocks
+  "Get all of the clocks that are recorded in the database."
+  [db]
   (map convert-clock (as-db/all-clocks db)))
 
 (defn group-clocks-by-day
+  "Create a hashmap with days as the key, and clocks that fall within that day as the value."
   [clocks]
   (group-by
    (fn [clock]
      (.toLocalDate ^LocalDateTime (:starttime clock)))
    clocks))
 
-(defn get-clock-at-time [db ^LocalDateTime datetime clock-in?]
+(defn get-clock-at-time
+  "Get a clock at a specific `datetime`. This will look at the day, hour, and
+  minute values of the object, and then look for everything between the
+  beginning, and end of that period."
+  [db ^LocalDateTime datetime clock-in?]
   ;; TODO: Assumes there will only be one clock at that time. This could fail if
   ;; this assumption is not true.
   (first
@@ -135,7 +169,9 @@
     (-> datetime (.withSecond 59) (.withNano 999999999))
     clock-in?)))
 
-(defn amend-clock [db clock-in? ^LocalDateTime oldtime ^LocalDateTime newtime]
+(defn amend-clock
+  "Find the clock out (or clock-in if `clock-in?` is true) at `oldtime`, and replace it with `newtime`."
+  [db clock-in? ^LocalDateTime oldtime ^LocalDateTime newtime]
   (let [to-amend (get-clock-at-time db oldtime clock-in?)]
     (if clock-in?
       (as-db/amend-clockin db {:newstarttime (to-epoch newtime)
@@ -144,7 +180,9 @@
                                 :clockoutid (:clockoutid to-amend)}))))
 
 ;; Returns duration.
-(defn sum-clocks [clocks]
+(defn sum-clocks
+  "Return the duration of all the clocks in `clocks`."
+  [clocks]
   (reduce #(.plus ^java.time.Duration %1
                   (Duration/between (:starttime %2)
                                     (:stoptime %2)))
