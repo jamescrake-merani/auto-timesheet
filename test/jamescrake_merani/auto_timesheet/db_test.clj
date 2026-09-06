4067e5e5bfa8d6d1cad40eec31a2b71055ff6c73
James Crake-Merani <james@jamescm.co.uk>
2026-09-06 20:20:10 +0100

    Expand setup-test to do the looping as well.

--- test/jamescrake_merani/auto_timesheet/db_test.clj
+++ test/jamescrake_merani/auto_timesheet/db_test.clj
@@ -105,20 +105,24 @@
     :period [(LocalDateTime/of 2026 6 11 10 00) (LocalDateTime/of 2026 6 11 14 00)]
     :expected-remaining-clocks 2}])
 
-(defn setup-test [db test-map]
-  (db-raw/create-category db {:name "test"})
-  (doseq [[clock-start clock-end] (:clocks test-map)]
-    (sut/manual-entry db clock-start clock-end "test")))
+(defn setup-test [test-data action-fn assert-fn]
+  (doseq [datum test-data]
+    (let [db (make-db)]
+      (db-raw/create-category db {:name "test"})
+      (doseq [[clock-start clock-end] (:clocks datum)]
+        (sut/manual-entry db clock-start clock-end "test"))
+      (action-fn db datum)
+      (assert-fn db datum))))
 
 (t/deftest deletion-test
-  (doseq [datum deletion-clock-test-data]
-    (let [db (make-db)]
-      (setup-test db datum)
-      (sut/delete-clocks db (first (:period datum)) (second (:period datum)))
-      (t/is (= (count (sut/all-clocks db)) (:expected-remaining-clocks datum)))
-      (t/is (= (count (sut/clocks-within-timeperiod db (first (:period datum))
-                                                    (second (:period datum))))
-               0)))))
+  (setup-test deletion-clock-test-data
+              (fn [db datum]
+                (sut/delete-clocks db (first (:period datum)) (second (:period datum))))
+              (fn [db datum]
+                (t/is (= (count (sut/all-clocks db)) (:expected-remaining-clocks datum)))
+                (t/is (= (count (sut/clocks-within-timeperiod db (first (:period datum))
+                                                              (second (:period datum))))
+                         0)))))
 
 (def within-day-clock-test-data
   [{:clocks [[(LocalDateTime/of 2026 6 11 9 00) (LocalDateTime/of 2026 6 11 12 00)]
@@ -140,10 +144,11 @@
     :expected-duration (Duration/ofHours 4)}])
 
 (t/deftest within-day-clocks
-  (doseq [datum within-day-clock-test-data]
-    (let [db (make-db)]
-      (setup-test db datum)
-      (t/is (= (sut/sum-clocks (sut/clocks-within-date db (LocalDate/of 2026 6 11))) (:expected-duration datum))))))
+  (setup-test
+   within-day-clock-test-data
+   (fn [db datum] nil)
+   (fn [db datum]
+     (t/is (= (sut/sum-clocks (sut/clocks-within-date db (LocalDate/of 2026 6 11))) (:expected-duration datum))))))
 
 (def amendment-test-data
   [{:clocks [[(LocalDateTime/of 2026 6 11 12 00 25) (LocalDateTime/of 2026 6 11 15 00)]]
@@ -200,11 +205,12 @@
                  [(LocalDateTime/of 2026 6 11 14 00) (LocalDateTime/of 2026 6 11 18 00)]]}])
 
 (t/deftest amendment-test
-  (doseq [datum amendment-test-data]
-    (let [db (make-db)]
-      (setup-test db datum)
-      (doseq [amendment (:amendments datum)]
-        (sut/amend-clock db (:clock-in? amendment) (:oldtime amendment) (:newtime amendment)))
-      (let [new-clocks (map #(vector (:starttime %) (:stoptime %)) (map sut/convert-clock (db-raw/all-clocks db)))]
-        (t/is (= (frequencies (:clocks-now datum))
-                 (frequencies new-clocks)))))))
+  (setup-test
+   amendment-test-data
+   (fn [db datum]
+     (doseq [amendment (:amendments datum)]
+       (sut/amend-clock db (:clock-in? amendment) (:oldtime amendment) (:newtime amendment))))
+   (fn [db datum]
+     (let [new-clocks (map #(vector (:starttime %) (:stoptime %)) (map sut/convert-clock (db-raw/all-clocks db)))]
+       (t/is (= (frequencies (:clocks-now datum))
+                (frequencies new-clocks)))))))
