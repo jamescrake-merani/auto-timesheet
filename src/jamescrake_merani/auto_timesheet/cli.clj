e6f9490c41c7ac013a91a6ec2160c2712861bdcf
James Crake-Merani <james@jamescm.co.uk>
2026-09-15 20:26:13 +0100

    Missing db argument.

--- src/jamescrake_merani/auto_timesheet/cli.clj
+++ src/jamescrake_merani/auto_timesheet/cli.clj
@@ -72,7 +72,7 @@
           (helpers/clock-out @db effective-datetime)
           (nil? category)
           (error-and-quit "You have multiple clock ins. You must resolve this ambiguity by specifying a category (with the --category flag).")
-          :else (helpers/clock-out @db (helpers/category-to-id category true) effective-datetime))
+          :else (helpers/clock-out @db (helpers/category-to-id @db category true) effective-datetime))
     (println (format "Clocked out. You have worked %s"
                      (format-duration (Duration/between time-since-clockin (LocalDateTime/now)))))))
 
