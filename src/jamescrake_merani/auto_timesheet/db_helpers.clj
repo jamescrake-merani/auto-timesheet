c95c01baa751c067bb21f4858e1f82664f3b3226
James Crake-Merani <james@jamescm.co.uk>
2026-09-25 20:49:02 +0100

    Whoops, substitution went too far.

--- src/jamescrake_merani/auto_timesheet/db_helpers.clj
+++ src/jamescrake_merani/auto_timesheet/db_helpers.clj
@@ -43,8 +43,8 @@
     (format "%s - %s | (%s)"
             (.toString (.toLocalTime start))
             (.toString (.toLocalTime end))
-            (format-date-time-range (.toLocalDate start)
-                                    (.toLocalDate end)))))
+            (format-date-range (.toLocalDate start)
+                               (.toLocalDate end)))))
 
 (defn- to-epoch
   "Converts `ldt` to seconds since the UTC epoch."
